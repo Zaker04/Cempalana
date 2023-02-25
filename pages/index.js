2b0cad297b2019ff03eeabc44483b3a0c7e928e2
Zaker04 <turtwigh@hotmail.com>
2023-02-25 14:10:26 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen m-0 p-0" id="cover">
+      <div className="flex justify-center items-center w-50 h-full">
+        <div className="p-8 border-4 border-black text-white text-bold text-center backdrop-blur-lg">
+          <h1 className="text-7xl mb-6">Cempalana</h1>
+          <p className="text-white text-bold text-3xl">
+            Conectando identidades con tecnología y el espiritu del cempazuchitl
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
